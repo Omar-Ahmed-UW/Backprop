3ed69012c52ba84ebd7ff6bcdd8623f69c2954af
Omar Ahmed <oahmed@uw.edu>
2023-02-18 16:07:13 -0800

    completed layer class & added to network class

--- BackpropLayer.m
+++ BackpropLayer.m
@@ -21,21 +21,35 @@ classdef BackpropLayer
             end        
         end
 
-        function [obj, a] = forwardprop(obj, P)
+        function [obj, a] = layerForward(obj, P)
             % uses vectorized approach to calculate inner product of all
             % weights with inputs then adds the correct biases.
+            
+            % TODO omar: Shouldn't this statement be obj.W' * P?
             n = (P*obj.W) + obj.b;
             a = logsig(n);
             obj.mostRecentN = n;
         end
 
-        function [obj, output] = layersensitivity(obj, x)
+        function [obj, output] = layerSensitivity(obj, x)
             % calculates the sensitivity for this layer
             % x represents the component from the following layer (either
             % t-a or the sum of sensitivity*weight for each neuron)
+            
+            % I think this equation isn't generalized for all layers, it is
+            % just for computing the output layer sensitivity. The
+            % generalized equation for all other layers doesn't include
+            % '-2'
             output = -2*(derivlogsig(obj.mostRecentN)*x);
         end
-
         
+        function [obj] = updateLayer(obj, learningRate, sensitivity, prevLayerOutput)
+            % updates the weights and bias of this layer using the
+            % learning rate sepcified in the network, the senstivity
+            % computed for this layer, and the layer output of the layer
+            % before it.
+            obj.W = obj.W - learningRate*sensitivity*(prevLayerOutput');
+            obj.b = obj.b - learningRate*sensitivity;        
+        end
     end
 end
--- BackpropNetwork.m
+++ BackpropNetwork.m
@@ -1,16 +1,19 @@
 classdef BackpropNetwork
     
     properties
+        % Backprop layer instances
         layer1
         layer2
 
         % easier to keep a in two different variables for now, because it's
         % a jagged array
-        %   possibly use "cell array" in the future?
+        %   TODO linnea: possibly use "cell array" in the future?
         a1
-        a2
-
-        s
+        a2        
+        
+        % storing sensitivies computed for each of the two layers
+        s1
+        s2
     end
     
     methods
@@ -22,12 +25,19 @@ classdef BackpropNetwork
             obj.layer2 = BackpropLayer(hidden, output);
         end
         
-        function [obj, temp2] = forward(obj,input)
+        function [obj, temp2] = networkForward(obj,input)
             % propogates outputs forward through all layers
             % returns the final output
-            [obj.layer1, obj.a1] = obj.layer1.forwardprop(input);
-            [obj.layer2, temp2] = obj.layer2.forwardprop(obj.a1);
+            [obj.layer1, obj.a1] = obj.layer1.layerForward(input);
+            [obj.layer2, temp2] = obj.layer2.layerForward(obj.a1);
             obj.a2 = temp2;
+        end
+
+        function [obj] = networkSensitivity(obj, targetOutput)
+            % computes the sensitivties for all layers in the network using
+            % the targetOutput for the output layer.
+            [obj.layer2, obj.s2] = obj.layer2.layerSensitivity(targetOutput - obj.a2);
+            [obj.layer1, obj.s1] = obj.layer1.layerSensitivity((obj.layer2.W'*obj.s2));
         end
     end
 end
