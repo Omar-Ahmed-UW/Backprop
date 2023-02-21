2f62d9cce95cfdeef9f3f53c64b853ecba06747c
Linnea Walsh <linnea.walsh@gmail.com>
2023-02-21 01:04:59 -0800

    Update part 2 driver

--- driver.m
+++ driver.m
@@ -1,7 +1,39 @@
 scores = zeros(1, 3);
 
 for i = 1:3
-    scores(i) = testPart2(10, 100, 50);
+    scores(i) = testPart2(10, 100, 20);
 end
 
+disp("20: ")
 disp(scores);
+disp("-------------------------")
+
+scores = zeros(1, 3);
+
+for i = 1:3
+    scores(i) = testPart2(10, 100, 30);
+end
+
+disp("30: ")
+disp(scores);
+disp("-------------------------")
+
+scores = zeros(1, 3);
+
+for i = 1:3
+    scores(i) = testPart2(10, 100, 40);
+end
+
+disp("40: ")
+disp(scores);
+disp("-------------------------")
+
+scores = zeros(1, 3);
+
+for i = 1:3
+    scores(i) = testPart2(10, 100, 32);
+end
+
+disp("32: ")
+disp(scores);
+disp("-------------------------")
